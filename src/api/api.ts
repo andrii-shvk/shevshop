83a02c794551b83ffe6e21dba07d899dd022c28a
Andrii Shevchuk <supamercron@gmail.com>
2024-11-05 14:41:54 -0500

    change vite version for deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,4 @@
+import { AuthResponse } from "@/models/response/authResponse";
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const $api = axios.create({
@@ -9,5 +10,31 @@ $api.interceptors.request.use((config: InternalAxiosRequestConfig<any>) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
     return config;
 });
+
+$api.interceptors.response.use(
+    (config) => {
+        return config;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+        if (
+            error.response.status == 401 &&
+            error.config &&
+            !error.config._isRetry
+        ) {
+            originalRequest.isRetry = true;
+            try {
+                const response = await $api.get<AuthResponse>("/refresh", {
+                    withCredentials: true,
+                });
+                localStorage.setItem("token", response.data.accessToken);
+                return $api.request(originalRequest);
+            } catch (e) {
+                console.log("User is not authorized");
+            }
+        }
+        throw error;
+    }
+);
 
 export { $api };
